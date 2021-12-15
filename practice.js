87fe8dccdc48572cd0ebcc25f41a230a7e9a7b90
Michael <Michaellovesellen@gmail.com>
2021-12-15 14:25:27 -0700

    Initial commit and changes

--- practice.js
+++ practice.js
@@ -1,2 +1,15 @@
-var name = "name";
-console.log(name)
+var age = 16;
+
+if(age >= 18){
+    console.log("Here's your ticket. Good luck!")
+}
+
+else{
+    console.log("Sorry! Come back when you're 18.")
+}
+
+var name = "Mike";
+if(name == "Mike"){
+
+}
+// 
